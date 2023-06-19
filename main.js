2e158213bbd6dce98ad2724249ac4f979812ac1a
Rebelzob <karimdavid07@gmail.com>
2023-06-20 00:17:27 +0100

    HTML CSS and JS

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-let burgerMenuToggle = document.querySelector('.mainMenu');
-let burgerMenu = document.querySelector('.cross-menu');
-let crossMenu = document.querySelector('.burger');
+let burgerMenuToggle = document.querySelector('.burger-container');
+let burgerMenu = document.querySelector('.burger');
+let crossMenu = document.querySelector('.cross-menu');
+
+burgerMenuToggle.addEventListener('click', () => {
+    burgerMenu.style.left = '0';
+})
+
+crossMenu.addEventListener('click', () => {
+    burgerMenu.style.left = '100%';
+})
